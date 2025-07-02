23b85960cab0bcc2331f24cebc2fd80a967ccaf2
AxelGautier14 <axel.gautier@epita.fr>
2025-07-02 15:07:34 +0200

    e

---
+++ web-api/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import gardenRoutes from './routes/gardenRoutes';
+
+const app = express();
+const PORT = 5173;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/gardens', gardenRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
