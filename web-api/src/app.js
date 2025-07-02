4957224d01b1a57723f0c2eb90896d9da63cd82e
AxelGautier14 <axel.gautier@epita.fr>
2025-07-02 16:13:39 +0200

    Update app.js

--- web-api/src/app.js
+++ web-api/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const gardensRouter = require('../controllers/gardens');
+const gardensRouter = require('controllers/gardens');
 
 const app = express();
 const PORT = 4000;
