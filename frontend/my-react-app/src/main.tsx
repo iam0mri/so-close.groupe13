e5f8a1b0cdc2e6ad09560f9177048a5f11b4a388
AxelGautier14 <axel.gautier@epita.fr>
2025-07-09 17:36:02 +0200

    Update main.tsx

--- frontend/my-react-app/src/main.tsx
+++ frontend/my-react-app/src/main.tsx
@@ -2,19 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = "dev-f4ersp6xmlisgq2s.us.auth0.com";
+const clientId = "vxLEx7Bcariz0SRq5EpZ4XBApPucGcSF";
+const audience = "https://dev-f4ersp6xmlisgq2s.us.auth0.com/api/v2/";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-f4ersp6xmlisgq2s.us.auth0.com"
-      clientId="vxLEx7Bcariz0SRq5EpZ4XBApPucGcSF"
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: "https://dev-f4ersp6xmlisgq2s.us.auth0.com/api/v2/",
-        scope: "openid profile email"
+        audience,
       }}
     >
       <App />
     </Auth0Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
